d24be236c22fa151f028c3f2c9f9707c93dbd24f
Aryan Kumar <anshsuroy143@gmail.com>
2026-06-15 18:10:45 +0530

    Fix Atlas database seeding

--- init/index.js
+++ init/index.js
@@ -1,33 +1,34 @@
+require("dotenv").config();
+
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const dbUrl = process.env.ATLAS_DB_URL;
 
 main()
-  .then(() => {
-    console.log("connected to db");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+    .then(() => {
+        console.log("Connected to Atlas");
+        initDB();
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 async function main() {
-  await mongoose.connect(MONGO_URL);
+    await mongoose.connect(dbUrl);
 }
-
 
 const initDB = async () => {
     await Listing.deleteMany({});
 
-    initData.data = initData.data.map((obj) => ({
+    const data = initData.data.map((obj) => ({
         ...obj,
-owner: "6a29398dd508ba895ae66a2f"    }));
+        owner: "6a29398dd508ba895ae66a2f"
+    }));
 
-    await Listing.insertMany(initData.data);
+    await Listing.insertMany(data);
 
     console.log("Database initialized");
+    mongoose.connection.close();
 };
-
-initDB();
-initDB();
